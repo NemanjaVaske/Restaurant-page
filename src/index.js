b3a6da824bb28782f272fda37939a5344ac9c778
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-03 20:07:52 +0100

    Add logic to switch between tabs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import { menu } from './menu';
 import { contact } from './contact';
 
 createNavbar();
-window.addEventListener('load',contact);
+window.addEventListener('load',home);
+const navbar = document.querySelector('.navbar').childNodes;
+navbar.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        if(item.textContent === 'Home'){
+            home();
+        }
+        else if(item.textContent === 'Menu'){
+            menu();
+        }
+        else{
+            contact();
+        }
+    });
+});
 
 
