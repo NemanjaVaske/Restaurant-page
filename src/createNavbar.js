7ddd7b831d17ec71d30fed67046cb33a037ad319
NemanjaVaske <nemanjavasic49@yahoo.com>
2022-03-03 15:31:25 +0100

    Create simple navbar

---
+++ src/createNavbar.js
@@ -0,0 +1,23 @@
+function createNavbar (){
+    const content = document.getElementById('content')
+    
+    const navbar = document.createElement('div')
+    navbar.classList.add("navbar")
+    const home = document.createElement('span');
+    const menu = document.createElement('span');
+    const contact = document.createElement('span');
+    home.classList.add('navbar-home');
+    menu.classList.add('navbar-menu');
+    contact.classList.add('navbar-contact');
+    home.textContent = "Home";
+    menu.textContent = "Menu";
+    contact.textContent = "Contact";
+
+    navbar.appendChild(home);
+    navbar.appendChild(menu);
+    navbar.appendChild(contact);
+
+    content.appendChild(navbar)
+}
+
+export {createNavbar}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("hello")
+import {createNavbar} from './createNavbar'
+
+createNavbar();
